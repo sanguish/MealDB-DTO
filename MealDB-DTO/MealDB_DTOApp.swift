6fb2c80afc9a7a25c14a112a59dbf1568509e1b7
Scott Anguish <sanguish@me.com>
2024-05-17 17:28:16 -0400

    Initial commit of the project

---
+++ MealDB-DTO/MealDB_DTOApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealDB_DTOApp.swift
+//  MealDB-DTO
+//
+//  Created by Scott Anguish on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MealDB_DTOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MealDB-DTO
-Example code that goes along with the article at 
+
+
+Example code that goes along with the article at https://sanguish.ghost.io/the-json-you-get
+
+Scott Anguish
+https://iosdev.space/@sanguish
